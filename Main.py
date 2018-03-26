3a50a6265e0abd1fb34cdf6990c0dbd98ef4a11c
jd-reynolds <johndentonreynolds@gmail.com>
2018-03-25 20:16:27 -0500

    Defined menu() with options: > newGame > settings > instructions > quit

--- Main.py
+++ Main.py
@@ -1,7 +1,10 @@
 import random
 
+title = "|\|\|\|    N-Back    |/|/|/|"
+subtitle = "  'the memory buffer game'  "
 length = 2                                              # defaults
 range = 3
+on = True
 
 def clearScreen():
     print('\n'*100)
@@ -27,7 +30,6 @@ def nBack(n, c):
             streak = (match == guess)
 
             if not streak:
-                clearScreen()
                 print("Incorrect: the buffer contains [" + buffStr + "]")
 
     print("You recalled " + str(count - n) + " matches correctly.")
@@ -37,7 +39,45 @@ def nBack(n, c):
     if again == 'y':
         nBack(n, c)
 
-nBack(length, range)
+def menu():
+    clearScreen()
+    print(title)
+    print(subtitle)
+    print("  length: " + str(length) + "   range: " + str(range))
+    print("[n]    new game")
+    print("[s]    settings")
+    print("[i]    instructions")
+    print("[q]    quit\n")
 
+    selection = input()
 
+    if selection == 'n':        # new game
+        nBack(length, range)
+
+    elif selection == 's':      # settings
+        clearScreen()
+        newLen = int(input("enter buffer length (1-20): \n"))
+
+        if 1 <= newLen <= 20:
+            global length
+            length = newLen
+        newRan = int(input("enter buffer range (2-9): \n"))
+
+        if 2 <= newRan <= 9:
+            global range
+            range = newRan
+
+    elif selection == 'i':      # instructions
+        clearScreen()
+        print("Numbers from 1-"+str(range)+" will be shown one at a time.")
+        print("If a number has appeared "+str(length)+" numbers ago,")
+        print("enter [m] for memory. \nOtherwise, enter [n] for no.")
+        input()
+
+    elif selection == 'q':      # quit
+        global on
+        on = False
+
+while on:
+    menu()
 
