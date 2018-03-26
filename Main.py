a6429d809e65a55dd45e9c134fd5898814ced1a0
jd-reynolds <johndentonreynolds@gmail.com>
2018-03-25 19:57:16 -0500

    defined nBack() added default length, range

--- Main.py
+++ Main.py
@@ -1,6 +1,37 @@
+import random
 
+length = 2                                              # defaults
+range = 3
 
+def nBack(n, c):
+    buffer = []
+    streak = True
+    count = 0
 
+    while streak:
+        count += 1
+        r = random.randint(1, c)
+        buffer = [r] + buffer
+        # clearScreen()
+        print(str(buffer[0]) + '\n')
+        guess = input()
+
+        if (count > n):
+            match = (buffer[0] == buffer[n])
+            buffStr = ' '.join(str(i) for i in buffer)
+            buffer.pop()
+            guess = (guess == 'm')
+            streak = (match == guess)
+
+            if not streak:
+                print("Incorrect: the buffer contains [" + buffStr + "]")
+
+    print("You recalled " + str(count - n) + " matches correctly.")
+    print("Play Again?\n[y]    yes\n[n]    no\n")
+    again = input()
+
+    if again == 'y':
+        nBack(n, c)
 
 
 
